57c614e3198e8f9aebbe06b605fc0c2afa6d9693
goodwinamundson <goodwinamundson@gmail.com>
2022-01-18 11:24:45 +0200

    added random order to fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -37,7 +37,14 @@ promptFight = promptFight.toLowerCase();
 
 // fight function (now with parameter for enemy's name)
 var fight = function(enemy) {
+  // keep track of who goes first
+var isPlayerTurn = true;
+// randomly change turn order
+if (Math.random() > 0.5) {
+  isPlayerTurn = false;
+} 
   while (playerInfo.health > 0 && enemy.health > 0) {
+    if (isPlayerTurn) {
     //ask player whether they'd like to fight or skip
     if (fightOrSkip()) {
       // if true, leave fight by breaking loop
@@ -81,6 +88,9 @@ var fight = function(enemy) {
     } else {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
+  }
+  // switch turn order for next round
+  isPlayerTurn = !isPlayerTurn;
   }
 };
 
